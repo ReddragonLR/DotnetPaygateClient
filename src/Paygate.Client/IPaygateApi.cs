b005baee1f8a54169abb50652af4e6917777113a
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2023-09-24 21:18:01 +0200

    fix: using default body serialization encoding

--- src/Paygate.Client/IPaygateApi.cs
+++ src/Paygate.Client/IPaygateApi.cs
@@ -6,9 +6,9 @@ namespace Paygate.Client
     internal interface IPaygateApi
     {
         [Post("/initiate.trans")]
-        Task<ApiResponse<string>> InitiateTransaction([Body(BodySerializationMethod.UrlEncoded)] Models.InitiatePayment.Request request);
+        Task<ApiResponse<string>> InitiateTransaction([Body(BodySerializationMethod.Default)] Models.InitiatePayment.Request request);
 
         [Post("/query.trans")]
-        Task<ApiResponse<string>> QueryTransaction([Body(BodySerializationMethod.UrlEncoded)] Models.QueryTransaction.Request request);
+        Task<ApiResponse<string>> QueryTransaction([Body(BodySerializationMethod.Default)] Models.QueryTransaction.Request request);
     }
 }
