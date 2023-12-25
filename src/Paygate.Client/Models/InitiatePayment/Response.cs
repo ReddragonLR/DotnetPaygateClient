7201302f2268ddc22ca445a3794f4f2d9dcffb3b
Lishen Ramsudh <lishen.ramsudh@gmail.com>
2023-12-25 15:31:53 +0200

    Adds form html to submit

--- src/Paygate.Client/Models/InitiatePayment/Response.cs
+++ src/Paygate.Client/Models/InitiatePayment/Response.cs
@@ -1,12 +1,13 @@
-﻿using Newtonsoft.Json;
-using Paygate.Client.Models.Common;
+﻿using Paygate.Client.Models.Common;
 using Refit;
+using System.Text;
 using System.Web;
 
 namespace Paygate.Client.Models.InitiatePayment
 {
     public sealed class Response
     {
+        const string INITIATE_PAYMENT_URL = "https://secure.paygate.co.za/payweb3/process.trans";
         public Response(string paygateId, string payRequestId, string paymentReference, string checksum)
         {
             PaygateId = paygateId;
@@ -30,16 +31,32 @@ namespace Paygate.Client.Models.InitiatePayment
         [AliasAs("CHECKSUM")]
         public string Checksum { get; set; }
 
+        public string FormHTML { get; set; }
+        public Uri InitiatePaymentUrl => new Uri(INITIATE_PAYMENT_URL);
+
         public static Response FromPayload(string payload)
         {
             var keyValues = HttpUtility.ParseQueryString(payload);
-            return new Response
+            var response = new Response
                 (
                     keyValues["PAYGATE_ID"],
                     keyValues["PAY_REQUEST_ID"],
                     keyValues["REFERENCE"],
                     keyValues["CHECKSUM"]
                 );
+            response.FormHTML = MapFormHTML(response.PayRequestId, response.Checksum, response.InitiatePaymentUrl);
+            return response;
+        }
+
+        private static string MapFormHTML(string payRequestId, string checksum, Uri initiatePaymentUrl)
+        {
+            StringBuilder htmlFormBuilder = new StringBuilder();
+            htmlFormBuilder.Append($"<form action=\"{initiatePaymentUrl.AbsolutePath}\" method=\"POST\">");
+            htmlFormBuilder.Append($"<input id=\"payrequestid\" name=\"PAY_REQUEST_ID\" value=\"{payRequestId}\">");
+            htmlFormBuilder.Append($"<input id=\"checksum\" name=\"CHECKSUM\" value=\"{checksum}\">");
+            htmlFormBuilder.Append("<input type=\"submit\">");
+            htmlFormBuilder.Append("</form>");
+            return htmlFormBuilder.ToString();
         }
     }
 }
